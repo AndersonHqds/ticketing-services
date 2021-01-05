73af04861cfe480a3e51129e6302da93d9009b89
Anderson Silva <anderson.silva@matera.com>
2021-01-04 22:21:03 -0300

    First commit

---
+++ auth/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { json } from "body-parser";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!");
+});
