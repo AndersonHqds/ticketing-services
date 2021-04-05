106b55392d715af324bd95a09225eb5d5e0fe34c
Anderson Silva <anderson.silva@matera.com>
2021-04-04 22:59:24 -0300

    Current user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -23,16 +23,28 @@ interface UserDoc extends mongoose.Document {
   createdAt: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 // Middleware before save
 userSchema.pre("save", async function (done) {
