2c245f99228b61b0954ca155ffd7c0031f24e5fb
Anderson Silva <anderson.silva@ab-inbev.com>
2022-04-05 01:32:17 -0300

    Adding more unit tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -18,6 +18,42 @@ it("Returns a 400 with an invalid email", async () => {
     .post(SIGNUP_URL)
     .send({
       email: "asdasd",
+      password: "p",
+    })
+    .expect(400);
+});
+
+it("Returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post(SIGNUP_URL)
+    .send({
+      email: "asdasd",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("Returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post(SIGNUP_URL)
+    .send({ email: "email@test.com" })
+    .expect(400);
+  await request(app).post(SIGNUP_URL).send({ password: "asdasda" }).expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post(SIGNUP_URL)
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post(SIGNUP_URL)
+    .send({
+      email: "test@test.com",
       password: "password",
     })
     .expect(400);
