34aa2ab4a2988ec64630222bd9a92d42b1d12510
Anderson Silva <anderson.silva@ab-inbev.com>
2022-05-19 00:37:30 -0300

    Creating use-request hook and updating signup page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,14 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser").catch(console.log);
+
+  return response.data;
 };
 
 export default LandingPage;
