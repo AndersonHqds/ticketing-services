5f25520a416e79ff0a5e503e8c372027c30fdb34
Anderson Silva <anderson.silva@ab-inbev.com>
2022-10-28 01:05:57 -0300

    Creating ticket service

---
+++ tickets/src/routes/__test/new.test.ts
@@ -0,0 +1,69 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/tickets";
+
+it("has a route handler listening to /api/tickets for post requests", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  expect(response.statusCode).not.toEqual(404);
+});
+
+it("can only be accessed if the user is signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  expect(response.statusCode).toEqual(401);
+});
+
+it("returns a status other than 401 if the user is signed in", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({});
+  expect(response.statusCode).not.toEqual(401);
+});
+
+it("returns an error if an invalid title is provided", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 10,
+    });
+  expect(response.statusCode).toEqual(400);
+});
+
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "asdasda",
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "asdasda",
+    })
+    .expect(400);
+});
+
+it("creates a ticket with valid inputs", async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+  const title = "asdasdasd";
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title,
+      price: 20,
+    })
+    .expect(201);
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
+});
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,49 @@
+import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
+  mongo = new MongoMemoryServer();
+  await mongo.start();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    id: "123123123",
+    email: "test@test.com",
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJson = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJson).toString("base64");
+  return [`express:sess=${base64}`];
+};
